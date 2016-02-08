680d8d7f58a78ec0f28b24ce90bb6bb0d337e682
Michael Teolis <michaelteolis96@gmail.com>
2016-02-08 18:22:12 -0500

    db terms not adding

--- terms.aspx.cs
+++ terms.aspx.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
 
 public partial class Terms : System.Web.UI.Page
 {
+    private OleDbConnection connection;
+    private OleDbCommand cmd = null;
+    private String path = @"Provider=Microsoft.ACE.OLEDB.12.0;DataSource=VanierFaces.accdb;Persist Security Info=False;";
+
+    private string temp = "temp";
+
     protected void Page_Load(object sender, EventArgs e)
     {
         this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
@@ -20,5 +27,13 @@ public partial class Terms : System.Web.UI.Page
     protected void onClick_accept(object sender, EventArgs e)
     {
         Server.Transfer("index.aspx");
+
+        OleDbDataReader reader;
+        connection = new OleDbConnection(path);
+        connection.Open();
+        cmd = new OleDbCommand("INSERT INTO tblProfiles (user_name, user_password, first_name, last_name, gender, dob, street_address, province, country, zip) "
+            + "VALUES ('" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "', '" + temp + "');", connection);
+        cmd.ExecuteNonQuery();
+        connection.Close();
     }
 }
