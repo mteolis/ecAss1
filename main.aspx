cd880c9d331dd45ff7cbcd17ead002eab7244505
Michael Teolis <michaelteolis96@gmail.com>
2016-02-08 22:02:30 -0500

    all

--- main.aspx
+++ main.aspx
@@ -31,6 +31,17 @@
                     <asp:ListItem Value="user1"></asp:ListItem>
                 </asp:BulletedList>
             </div>
+            <asp:GridView ID="GridViewProfile" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="Solid" BorderWidth="1px" CellPadding="3" ForeColor="Black" GridLines="Vertical" Height="66px" Width="1264px">
+                <AlternatingRowStyle BackColor="#CCCCCC" />
+                <FooterStyle BackColor="#CCCCCC" />
+                <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#808080" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#383838" />
+            </asp:GridView>
         </form>
     </body>
 </html>
--- myprofile.aspx.cs
+++ myprofile.aspx.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
 
 public partial class myprofile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+
+
+        OleDbConnection myConnection = default(OleDbConnection);
+        OleDbCommand myCommand = default(OleDbCommand);
+        string strSQL = null;
+        strSQL = "SELECT user_name, user_password, first_name, last_name, gender, dob, street_address, province, country FROM tblProfiles WHERE user_name = 'Randolph'";
+        myConnection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + System.Web.HttpContext.Current.Server.MapPath("VanierFaces.accdb") + ";Persist Security Info=False;");
+        myConnection.Open();
+        myCommand = new OleDbCommand(strSQL, myConnection);
+
+        OleDbDataAdapter adp = new OleDbDataAdapter(myCommand);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+        GridViewMyProfile.DataSource = ds;
+        GridViewMyProfile.DataBind();
     }
 
     protected void onClick_cancel(object sender, EventArgs e)
